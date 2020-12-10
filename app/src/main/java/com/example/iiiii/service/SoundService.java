f8e1cb33206d75f2bfc9a5c9295485705bd01785
OtaKatsui <x18g012@chiba-fjb.ac.jp>
2020-12-10 15:06:49 +0900

    k

--- app/src/main/java/com/example/iiiii/service/SoundService.java
+++ app/src/main/java/com/example/iiiii/service/SoundService.java
@@ -6,13 +6,10 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.AsyncTask;
-
 import com.example.iiiii.R;
 
 
 import java.util.Random;
-
-import static android.os.Build.VERSION_CODES.R;
 
 
 public class SoundService extends AsyncTask<Integer, Integer, Integer> {
@@ -31,38 +28,12 @@ public class SoundService extends AsyncTask<Integer, Integer, Integer> {
 //        sound[2] = soundPool.load(context, R.raw.sound3, 1);
 //        sound[3] = soundPool.load(context, R.raw.sound4, 1);
 //        sound[4] = soundPool.load(context, R.raw.sound5, 1);
-        soundPool.play(sound[0], 1.0f, 1.0f, 0, 0, 1.0f);
     }
 
     @Override
     protected Integer doInBackground(Integer... params) {
 
-
-//        for(int i=0; i<10; i++){
-//            int r1 = r.nextInt(3);
-//            switch (r1){
-//                case 0:
-//                    soundPool.play(sound[0], 1.0f, 1.0f, 0, 0, 1.0f);
-//                    break;
-//                case 1:
-//                    soundPool.play(sound[1], 1.0f, 1.0f, 0, 0, 1.0f);
-//                    break;
-//                case 2:
-//                    soundPool.play(sound[2], 1.0f, 1.0f, 0, 0, 1.0f);
-//                    break;
-//                case 3:
-//                    soundPool.play(sound[3], 1.0f, 1.0f, 0, 0, 1.0f);
-//                    break;
-//                case 4:
-//                    soundPool.play(sound[4], 1.0f, 1.0f, 0, 0, 1.0f);
-//                    break;
-//            }
-//        }
-
-
-
         soundPool.play(sound[0], 1.0f, 1.0f, 0, 0, 1.0f);
-
 //        try {
 //            Thread.sleep(1000);
 //        } catch (InterruptedException e) {
